9d5a02e3b681cd83ecc7590b93da4e1b71c7117f
Simon Whitty <simon@whileloop.com>
2016-12-13 09:26:44 +1100

    Initial Commit

---
+++ SwiftVGTests/SwiftVGTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftVGTests.swift
+//  SwiftVGTests
+//
+//  Created by Simon Whitty on 13/12/16.
+//  Copyright © 2016 WhileLoop Pty Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftVG
+
+class SwiftVGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
