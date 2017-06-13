f8585219ce59cd73079384523cfec021510a201b
Simon Whitty <simon@whileloop.com>
2017-06-13 18:57:15 +1000

    adding Transform equality tests

--- SwiftDrawTests/LayerTree/LayerTree.TransformTests.swift
+++ SwiftDrawTests/LayerTree/LayerTree.TransformTests.swift
@@ -31,22 +31,47 @@ import XCTest
 
 class LayerTreeTransformTests: XCTestCase {
     
+    typealias Transform = LayerTree.Transform
     typealias Matrix = LayerTree.Transform.Matrix
     
-    func testTransformEquality() {
-        let t1 = Matrix(a: 0, b: 1, c: 2, d: 3, tx: 4, ty: 5)
-        let t2 = Matrix(a: 5, b: 4, c: 3, d: 2, tx: 1, ty: 0)
-        let t3 = LayerTree.Transform.identity.toMatrix()
+    func testMatrixEquality() {
+        let m1 = Matrix(a: 0, b: 1, c: 2, d: 3, tx: 4, ty: 5)
+        let m2 = Matrix(a: 5, b: 4, c: 3, d: 2, tx: 1, ty: 0)
+        let m3 = Transform.identity.toMatrix()
         
-        XCTAssertEqual(t1, Matrix(a: 0, b: 1, c: 2, d: 3, tx: 4, ty: 5))
+        XCTAssertEqual(m1, Matrix(a: 0, b: 1, c: 2, d: 3, tx: 4, ty: 5))
+        XCTAssertEqual(m1, m1)
+        XCTAssertEqual(m2, Matrix(a: 5, b: 4, c: 3, d: 2, tx: 1, ty: 0))
+        XCTAssertEqual(m2, m2)
+        XCTAssertEqual(m3, Matrix(a: 1, b: 0, c: 0, d: 1, tx: 0, ty: 0))
+        XCTAssertEqual(m3, m3)
+        
+        XCTAssertNotEqual(m1, m2)
+        XCTAssertNotEqual(m1, m3)
+        XCTAssertNotEqual(m2, m3)
+    }
+    
+    func testTransfromEquality() {
+        let m1 = Matrix(a: 0, b: 1, c: 2, d: 3, tx: 4, ty: 5)
+        let t1 = Transform.matrix(m1)
+        let t2 = Transform.translate(tx: 1, ty: 2)
+        let t3 = Transform.scale(sx: 3, sy: 4)
+        let t4 = Transform.rotate(radians: Float.pi/2)
+        
+        XCTAssertEqual(t1, .matrix(m1))
         XCTAssertEqual(t1, t1)
-        XCTAssertEqual(t2, Matrix(a: 5, b: 4, c: 3, d: 2, tx: 1, ty: 0))
+        XCTAssertEqual(t2, .translate(tx: 1, ty: 2))
         XCTAssertEqual(t2, t2)
-        XCTAssertEqual(t3, Matrix(a: 1, b: 0, c: 0, d: 1, tx: 0, ty: 0))
+        XCTAssertEqual(t3, .scale(sx: 3, sy: 4))
         XCTAssertEqual(t3, t3)
+        XCTAssertEqual(t4, .rotate(radians: Float.pi/2))
+        XCTAssertEqual(t4, t4)
         
         XCTAssertNotEqual(t1, t2)
         XCTAssertNotEqual(t1, t3)
+        XCTAssertNotEqual(t1, t4)
         XCTAssertNotEqual(t2, t3)
+        XCTAssertNotEqual(t2, t4)
+        XCTAssertNotEqual(t3, t4)
     }
 }
