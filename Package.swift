ba064571d0176857df0c740a2713e77f19d120e0
Simon Whitty <simon@whileloop.com>
2018-11-25 09:20:33 +1100

    [gardening]

--- Package.swift
+++ Package.swift
@@ -18,8 +18,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftDraw",
             dependencies: [],
