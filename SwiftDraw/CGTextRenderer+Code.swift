51f067e28c1f42ed18c7cc795c4b78f9a9ab1a75
Simon Whitty <simon@whileloop.com>
2022-02-26 10:01:57 +1100

    Data Constructor

--- SwiftDraw/CGTextRenderer+Code.swift
+++ SwiftDraw/CGTextRenderer+Code.swift
@@ -33,49 +33,62 @@ import Foundation
 
 public extension CGTextRenderer {
 
-  typealias Size = (width: Int, height: Int)
+    typealias Size = (width: Int, height: Int)
 
-  static func render(named name: String, in bundle: Bundle = Bundle.main, size: Size? = nil) -> String? {
-    guard let url = bundle.url(forResource: name, withExtension: nil) else { return nil }
-    return render(fileURL: url, size: size)
-  }
-
-  static func render(fileURL: URL, size: Size? = nil) -> String? {
-    guard let svg = try? DOM.SVG.parse(fileURL: fileURL) else {
-      return nil
+    static func render(named name: String, in bundle: Bundle = Bundle.main, size: Size? = nil) -> String? {
+        guard let url = bundle.url(forResource: name, withExtension: nil) else { return nil }
+        return render(fileURL: url, size: size)
     }
 
-    let size = makeSize(svg: svg, size: size)
-    return cgCodeText(url: fileURL, svg: svg, size: size)
-  }
+    static func render(fileURL: URL, size: Size? = nil) -> String? {
+        guard let svg = try? DOM.SVG.parse(fileURL: fileURL) else {
+            return nil
+        }
 
-  private static func makeSize(svg: DOM.SVG, size: Size?) -> LayerTree.Size {
-    guard let size = size else {
-        return LayerTree.Size(svg.width, svg.height)
+        let size = makeSize(svg: svg, size: size)
+
+        let identifier = fileURL.lastPathComponent
+            .replacingOccurrences(of: ".\(fileURL.pathExtension)", with: "")
+            .replacingOccurrences(of: "-", with: " ")
+            .capitalized
+            .replacingOccurrences(of: " ", with: "")
+
+        return cgCodeText(name: "svg\(identifier)", svg: svg, size: size)
     }
-    return LayerTree.Size(LayerTree.Float(size.width), LayerTree.Float(size.height))
-  }
 
-  private static func cgCodeText(url: URL, svg: DOM.SVG, size: LayerTree.Size) -> String {
-    let layer = LayerTree.Builder(svg: svg).makeLayer()
-    let commandSize = LayerTree.Size(svg.width, svg.height)
-    let generator = LayerTree.CommandGenerator(provider: CGTextProvider(),
-                                               size: commandSize)
-    
-    let optimizer = LayerTree.CommandOptimizer<CGTextTypes>(options: [.skipRedundantState, .skipInitialSaveState])
-    let commands = optimizer.optimizeCommands(
-      generator.renderCommands(for: layer)
-    )
+    static func render(data: Data) throws -> String {
+        let svg = try DOM.SVG.parse(data: data)
+        let size = makeSize(svg: svg, size: nil)
+        return cgCodeText(name: "svgImage", svg: svg, size: size)
+    }
 
-    let identifier = url.lastPathComponent
-      .replacingOccurrences(of: ".\(url.pathExtension)", with: "")
-      .replacingOccurrences(of: "-", with: " ")
-      .capitalized
-      .replacingOccurrences(of: " ", with: "")
+    static func renderPath(from svgPath: String) throws -> String {
+        let domPath = try XMLParser().parsePath(from: svgPath)
+        let layerPath = try LayerTree.Builder.createPath(from: domPath)
+        return renderPath(from: layerPath)
+    }
 
-    let renderer = CGTextRenderer(name:"svg\(identifier)", size: size, commandSize: commandSize)
-    renderer.perform(commands)
-    
-    return renderer.makeText()
-  }
+    private static func makeSize(svg: DOM.SVG, size: Size?) -> LayerTree.Size {
+        guard let size = size else {
+            return LayerTree.Size(svg.width, svg.height)
+        }
+        return LayerTree.Size(LayerTree.Float(size.width), LayerTree.Float(size.height))
+    }
+
+    private static func cgCodeText(name: String, svg: DOM.SVG, size: LayerTree.Size) -> String {
+        let layer = LayerTree.Builder(svg: svg).makeLayer()
+        let commandSize = LayerTree.Size(svg.width, svg.height)
+        let generator = LayerTree.CommandGenerator(provider: CGTextProvider(),
+                                                   size: commandSize)
+
+        let optimizer = LayerTree.CommandOptimizer<CGTextTypes>(options: [.skipRedundantState, .skipInitialSaveState])
+        let commands = optimizer.optimizeCommands(
+            generator.renderCommands(for: layer)
+        )
+
+        let renderer = CGTextRenderer(name: name, size: size, commandSize: commandSize)
+        renderer.perform(commands)
+
+        return renderer.makeText()
+    }
 }
