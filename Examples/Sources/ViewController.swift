408b1f37006a48a2acaff82bda85bc78c81ee00c
Simon Whitty <simon@whileloop.com>
2022-08-05 08:43:41 +1000

    Fill Rule Sample

--- Examples/Sources/ViewController.swift
+++ Examples/Sources/ViewController.swift
@@ -65,7 +65,7 @@ class ViewController: UIViewController {
 
     override func loadView() {
         let imageView = UIImageView(frame: UIScreen.main.bounds)
-        imageView.image = Image(named: "path-close.svg", in: .samples)?.rasterize()
+        imageView.image = Image(named: "fill-rule.svg", in: .samples)?.rasterize()
         imageView.contentMode = .scaleAspectFit
         imageView.backgroundColor = .white
         self.view = imageView
