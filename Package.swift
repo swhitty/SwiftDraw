1201e6cca64dc88a4982251e94a5a04ff7da5caa
Simon Whitty <simon@whileloop.com>
2022-10-08 07:01:42 +1100

    0.13.2

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftDraw",
 	  platforms: [
-	       .macOS(.v10_12), .iOS(.v10),
-	    ],
+        .iOS(.v10), .macOS(.v10_12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(name: "swiftdraw", targets: ["CommandLine"]),
