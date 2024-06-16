3e88354ad8ea853dc58d237590944eaea0d559d6
Simon Whitty <simon@whileloop.com>
2024-06-16 19:24:25 +1000

    watchOS

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftDraw",
-	  platforms: [
-        .iOS(.v13), .macOS(.v10_15)
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(name: "swiftdraw", targets: ["CommandLine"]),
-		.library(
+        .library(
             name: "SwiftDraw",
             targets: ["SwiftDraw"]),
     ],
@@ -19,13 +19,13 @@ let package = Package(
         .target(
             name: "SwiftDraw",
             dependencies: [],
-			path: "SwiftDraw"
-		),
+            path: "SwiftDraw"
+        ),
         .executableTarget(
             name: "CommandLine",
             dependencies: ["SwiftDraw"],
             path: "CommandLine"
-		),
+        ),
         .testTarget(
             name: "SwiftDrawTests",
             dependencies: ["SwiftDraw"],
@@ -33,6 +33,6 @@ let package = Package(
             resources: [
                 .copy("Test.bundle")
             ]
-		)
+        )
     ]
 )
