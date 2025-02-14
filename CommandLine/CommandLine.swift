2cfd97c753feafe23c767ad3cb4daaf5c5415272
Simon Whitty <simon@whileloop.com>
2025-02-14 18:20:56 +1100

    0.18.3

--- CommandLine/CommandLine.swift
+++ CommandLine/CommandLine.swift
@@ -68,7 +68,7 @@ extension SwiftDraw.CommandLine {
     static func printHelp() {
         print("")
         print("""
-swiftdraw, version 0.18.2
+swiftdraw, version 0.18.3
 copyright (c) 2025 Simon Whitty
 
 usage: swiftdraw <file.svg> [--format png | pdf | jpeg | swift | sfsymbol] [--size wxh] [--scale 1x | 2x | 3x]
