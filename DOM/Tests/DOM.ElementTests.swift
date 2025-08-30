a519f08b63fc978288cb907dd90992c665c08b02
Simon Whitty <simon@whileloop.com>
2025-08-30 13:15:51 +1000

    swift testing

--- DOM/Tests/DOM.ElementTests.swift
+++ DOM/Tests/DOM.ElementTests.swift
@@ -29,144 +29,152 @@
 //  3. This notice may not be removed or altered from any source distribution.
 //
 
-import XCTest
+import Testing
 @testable import SwiftDrawDOM
 
-final class DOMElementTests: XCTestCase {
-  
-  func testLine() {
-    let element = DOM.createLine()
-    var another = DOM.createLine()
-    
-    XCTAssertEqual(element, another)
-    
-    another.x1 = 1
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createLine()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testCircle() {
-    let element = DOM.createCircle()
-    var another = DOM.createCircle()
-    
-    XCTAssertEqual(element, another)
-    
-    another.cx = 1
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createCircle()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testEllipse() {
-    let element = DOM.createEllipse()
-    var another = DOM.createEllipse()
-    
-    XCTAssertEqual(element, another)
-    
-    another.cx = 1
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createEllipse()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testRect() {
-    let element = DOM.createRect()
-    var another = DOM.createRect()
-    
-    XCTAssertEqual(element, another)
-    
-    another.x = 1
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createRect()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testPolygon() {
-    let element = DOM.createPolygon()
-    var another = DOM.createPolygon()
-    
-    XCTAssertEqual(element, another)
-    
-    another.points.append(DOM.Point(6, 7))
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createPolygon()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testPolyline() {
-    let element = DOM.createPolyline()
-    var another = DOM.createPolyline()
-    
-    XCTAssertEqual(element, another)
-    
-    another.points.append(DOM.Point(6, 7))
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createPolyline()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testText() {
-    let element = DOM.createText()
-    var another = DOM.createText()
-    
-    XCTAssertEqual(element, another)
-    
-    another.value = "Simon"
-    XCTAssertNotEqual(element, another)
-    
-    another = DOM.createText()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(element, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(element, another)
-  }
-  
-  func testGroup() {
-    let group = DOM.createGroup()
-    var another = DOM.createGroup()
-    
-    XCTAssertEqual(group, another)
-    
-    another.childElements.append(DOM.createCircle())
-    XCTAssertNotEqual(group, another)
-    
-    another = DOM.createGroup()
-    another.attributes.fill = .color(.keyword(.black))
-    XCTAssertNotEqual(group, another)
-    
-    another.attributes.fill = nil
-    XCTAssertEqual(group, another)
-  }
+struct DOMElementTests {
+
+    @Test
+    func line() {
+        let element = DOM.createLine()
+        var another = DOM.createLine()
+
+        #expect(element == another)
+
+        another.x1 = 1
+        #expect(element != another)
+
+        another = DOM.createLine()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func circle() {
+        let element = DOM.createCircle()
+        var another = DOM.createCircle()
+
+        #expect(element == another)
+
+        another.cx = 1
+        #expect(element != another)
+
+        another = DOM.createCircle()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func ellipse() {
+        let element = DOM.createEllipse()
+        var another = DOM.createEllipse()
+
+        #expect(element == another)
+
+        another.cx = 1
+        #expect(element != another)
+
+        another = DOM.createEllipse()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func rect() {
+        let element = DOM.createRect()
+        var another = DOM.createRect()
+
+        #expect(element == another)
+
+        another.x = 1
+        #expect(element != another)
+
+        another = DOM.createRect()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func polygon() {
+        let element = DOM.createPolygon()
+        var another = DOM.createPolygon()
+
+        #expect(element == another)
+
+        another.points.append(DOM.Point(6, 7))
+        #expect(element != another)
+
+        another = DOM.createPolygon()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func polyline() {
+        let element = DOM.createPolyline()
+        var another = DOM.createPolyline()
+
+        #expect(element == another)
+
+        another.points.append(DOM.Point(6, 7))
+        #expect(element != another)
+
+        another = DOM.createPolyline()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func text() {
+        let element = DOM.createText()
+        var another = DOM.createText()
+
+        #expect(element == another)
+
+        another.value = "Simon"
+        #expect(element != another)
+
+        another = DOM.createText()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(element != another)
+
+        another.attributes.fill = nil
+        #expect(element == another)
+    }
+
+    @Test
+    func group() {
+        let group = DOM.createGroup()
+        var another = DOM.createGroup()
+
+        #expect(group == another)
+
+        another.childElements.append(DOM.createCircle())
+        #expect(group != another)
+
+        another = DOM.createGroup()
+        another.attributes.fill = .color(.keyword(.black))
+        #expect(group != another)
+
+        another.attributes.fill = nil
+        #expect(group == another)
+    }
 }
